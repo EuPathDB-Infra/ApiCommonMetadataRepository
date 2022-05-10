2453128fcac180704705631017caba0938c1630a
Wojtek Bazant <wbazant@vet.upenn.edu>
2022-05-10 07:28:47 -0400

    List variables in owl, again

--- scripts/list_variables_in_owl.pl
+++ scripts/list_variables_in_owl.pl
@@ -5,6 +5,7 @@ use lib "$ENV{GUS_HOME}/lib/perl";
 use YAML;
 use ApiCommonData::Load::OntologyMapping;
 use feature 'say';
+use List::Util qw/uniq/;
 
 die "Usage: $0 owl" unless @ARGV;
 
@@ -14,6 +15,7 @@ my $om = ApiCommonData::Load::OntologyMapping->fromOwl(shift @ARGV);
 my ($ontologySources, $ontologyMapping) = $om->asSourcesAndMapping();
 
 my @a = map {$ontologyMapping->{$_}} sort keys %{$ontologyMapping};
-say Dump @a;
+say for uniq(sort grep {/::/} map {@{$_->{characteristicQualifier}{name}//[]}} @a);
+#say Dump @a;
 
 
